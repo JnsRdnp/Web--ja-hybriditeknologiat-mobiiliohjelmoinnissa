b6a114e70faca40fc16fc04b02613fe679a18464
JnsRdnp <112471004+JnsRdnp@users.noreply.github.com>
2024-01-08 14:59:11 +0200

    currencyChangerready

--- tuntiharjoitukset/demo/App.js
+++ tuntiharjoitukset/demo/App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  
+  const [euros,setEuros] = useState('');
+  const [pounds,setPounds] = useState(0);
+
+  const convertCurrency = (euro) => {
+    setPounds(euro*0.86);
+  }
+   
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Euros</Text>
+      <TextInput 
+        style={styles.field}
+
+        value={euros}
+        onChangeText={text => setEuros(text)}
+        keyboardType='decimal-pad'>
+      </TextInput>
+
+      <Text style={styles.field}>Pounds</Text>
+      <Text style={styles.field}>{pounds.toFixed(2)}</Text>
+
+      <Button title='Calculate' onPress={() => convertCurrency(euros)}></Button>
+
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -14,7 +37,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    //x
     alignItems: 'center',
+    //y
     justifyContent: 'center',
   },
+  field:{
+    marginBottom : '5%',
+
+  }
 });
